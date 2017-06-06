ab670f8d02f5a35e86b26cde2ad25e616b7e72ea
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-06-06 23:30:02 +0900

    third commit

--- src/reducers/foodReducer.js
+++ src/reducers/foodReducer.js
@@ -1,6 +1,4 @@
 import {
-  FOOD_CHANGE,
-  INGR_CHANGE,
   SET_OBJ
 
 } from '../actions/types';
@@ -12,12 +10,6 @@ const INITIAL_STATE = fakeData
 export default (state = INITIAL_STATE, action) => {
 
     switch(action.type){
-      
-      case FOOD_CHANGE:
-            return {...state, food: action.payload};
-    
-      case INGR_CHANGE:
-            return {...state, ingredients: action.payload};
       
       case SET_OBJ:
             return {...state, food: action.payload.food, ingredients: action.payload.ingredients};
